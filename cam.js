c8df7ed9a4dd62b0446b873856d477cb64e659b1
SriSabarish2021 <142215318+SriSabarish2021@users.noreply.github.com>
2024-05-21 08:19:19 +0530

    Update cam.js

--- cam.js
+++ cam.js
@@ -12,28 +12,5 @@ function getvid(){
         console.log(error)
     })
 }
-
-
-function canvas(){
-    const width=video.videoWidth;
-    const height=video.videoHeight;
-    canva.height=height
-    canva.width=width
-    return setInterval(()=>{
-        ctx.drawIMage(video,0,0,width,height)
-    },16)
-}
-
-function striping(){
-    snap.currentTime=0
-    snap.play()
-
-    const data=canva.toDataURL('image/jpeg')
-    let link=document.createElement('a')
-    link.href=data
-    link.setAttribute('download','handsome')
-    link.innerHTML=`img src="${data}" alt="handsome image"/`
-    strip.insertBefore(link,strip.firstChild)
-}
 getvid()
-video.addEventListener('canplay',canvas)
+
